72a2d647e3d60fa7c50307b3e5324adc47f575af
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-02-23 23:57:45 +0700

    Updates.

--- public/js/app.js
+++ public/js/app.js
@@ -77453,8 +77453,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/atomicbomber/experiments/drug-distribution-information-system/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/atomicbomber/experiments/drug-distribution-information-system/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/atomicbomber/projects/drug-distributor-information-system/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/atomicbomber/projects/drug-distributor-information-system/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
